661b8cdde42352146068fea66beafc9b97c13cc0
Kepler68 <alexanderculkin@gmail.com>
2017-02-13 15:41:34 -0500

    Keplers toast
    NOT DONE YET, this is a branch, a separate copy of the mod with edits I have made. once I finish with toast you can see it and merge it with the master branch if you approve

---
+++ common/mod/sparkyfox/servermod/item/ItemToast.java
@@ -0,0 +1,16 @@
+package item;
+
+//import com.kepler68.example.Reference;
+//import com.kepler68.example.example;
+
+import net.minecraft.item.Item;
+
+//public class ItemToast extends Item {
+
+//	public ItemObsidianingot()
+//	{
+//		setUnlocalizedName(Reference.ExampleItems.OBSIDIANINGOT.getUnlocalizedName());
+//		setRegistryName(Reference.ExampleItems.OBSIDIANINGOT.getRegistryName());
+//		setCreativeTab(example.tabExample);
+//	}
+//}
